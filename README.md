74d25957444778660f09cd5b18f5c37af06027a9
Dain Blodorn Kim <dain@db13.us>
2021-12-09 14:24:00 -0800

    fix

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Dains Nfts on top of erc721-gwei-slim usage contracts
 
 MrDainNFT = 628284 gas
+
+deploying "BleulandFollies" (tx: 0x56ff1ff227e1d67934fde94a937f758520f286204b0065e2b7933c7044dfa9e5)...: deployed at 0x8328Ac7E9cb02D85f1643C6Afe552a301adb2c9D with 622853 gas
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
       // this is the erc721base logic contract deployment
       1: '0x43955024b1985E2b933A59021500aE5f55b04091',
       // this is the erc721base logic contract deployment
-      4: '0x3be479a0e8d5732be5051bfe6833cc98722f2c1b',
+      4: '0x86c67a16C16BF784BdFE7D4b7575dB664D191F88',
     }
   },
   networks,
